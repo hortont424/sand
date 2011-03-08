3492f2220531158edd53532b715776549ed6a4d7
Tim Horton <hortont424@gmail.com>
2011-03-08 15:47:41 -0500

    point player towards mouse

--- Sand/Sand/Player.cs
+++ Sand/Sand/Player.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Net;
@@ -9,6 +10,7 @@ namespace Sand
     {
         internal Vector2 _position;
         internal SpriteBatch _spriteBatch;
+        internal float _angle;
 
         public Player(Game game) : base(game)
         {
@@ -24,7 +26,8 @@ namespace Sand
         public override void Draw(GameTime gameTime)
         {
             _spriteBatch.Begin();
-            _spriteBatch.Draw(Storage.Sprite("pixel"), new Rectangle((int)_position.X, (int)_position.Y, 5, 20), Color.Red);
+            _spriteBatch.Draw(Storage.Sprite("pixel"), new Rectangle((int) _position.X, (int) _position.Y, 5, 20), null,
+                              Color.Red, _angle, new Vector2(0.5f, 0.5f), SpriteEffects.None, 0.0f);
             _spriteBatch.End();
         }
     }
@@ -52,7 +55,7 @@ namespace Sand
         private Vector2 _drag;
         private Vector2 _movementAcceleration;
         private KeyboardState _oldKeyState;
-        
+
         private Vector2 _velocity;
 
         public LocalPlayer(Game game) : base(game)
@@ -65,25 +68,14 @@ namespace Sand
         {
             base.Update(gameTime);
 
-            var timestep = (float)(gameTime.ElapsedGameTime.TotalMilliseconds / (1000 / 60));
-
             UpdateInput();
-
-            _acceleration.X -= _drag.X * _velocity.X;
-            _acceleration.Y -= _drag.Y * _velocity.Y;
-
-            _velocity.X += _acceleration.X;
-            _velocity.Y += _acceleration.Y;
-
-            _position.X += _velocity.X * timestep;
-            _position.Y += _velocity.Y * timestep;
-
-            _acceleration.X = _acceleration.Y = 0.0f;
+            UpdateAngle();
+            UpdatePosition(gameTime);
         }
 
         private void UpdateInput()
         {
-            KeyboardState newKeyState = Keyboard.GetState();
+            var newKeyState = Keyboard.GetState();
 
             if(newKeyState.IsKeyDown(Keys.A))
             {
@@ -104,6 +96,29 @@ namespace Sand
             }
 
             _oldKeyState = newKeyState;
+        }
+
+        private void UpdateAngle()
+        {
+            var mouseState = Mouse.GetState();
+
+            _angle = (float)Math.Atan2(mouseState.Y - _position.Y, mouseState.X - _position.X) + ((float)Math.PI / 2.0f);
+        }
+
+        private void UpdatePosition(GameTime gameTime)
+        {
+            var timestep = (float)(gameTime.ElapsedGameTime.TotalMilliseconds / (1000 / 60));
+
+            _acceleration.X -= _drag.X * _velocity.X;
+            _acceleration.Y -= _drag.Y * _velocity.Y;
+
+            _velocity.X += _acceleration.X;
+            _velocity.Y += _acceleration.Y;
+
+            _position.X += _velocity.X * timestep;
+            _position.Y += _velocity.Y * timestep;
+
+            _acceleration.X = _acceleration.Y = 0.0f;
         }
     }
 }
