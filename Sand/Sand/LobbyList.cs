38aa21ef2831f79c75e6ab06d44913a806f00d84
Tim Horton <hortont424@gmail.com>
2011-03-08 18:03:50 -0500

    show latency

--- Sand/Sand/LobbyList.cs
+++ Sand/Sand/LobbyList.cs
@@ -28,14 +28,11 @@ namespace Sand
 
             _spriteBatch.Begin();
 
-            /*_spriteBatch.DrawString(Storage.Font("Calibri48Bold"), "SAND",
-                                    new Vector2(Game.Window.ClientBounds.Width * 0.2f, 20), Color.White);*/
-
             _spriteBatch.Draw(Storage.Sprite("SandLogo"), new Vector2(Game.Window.ClientBounds.Width * 0.2f, 20), Color.White);
 
             foreach(var gamer in Storage.networkSession.AllGamers)
             {
-                _spriteBatch.DrawString(Storage.Font("Calibri24"), gamer.Gamertag,
+                _spriteBatch.DrawString(Storage.Font("Calibri24"), string.Format("{0} ({1} ms)", gamer.Gamertag, gamer.RoundtripTime.TotalMilliseconds),
                                         new Vector2(Game.Window.ClientBounds.Width * 0.22f, i++ * 30 + 120),
                                         Color.White);
             }
