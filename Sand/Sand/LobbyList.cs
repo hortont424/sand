3daf2777d6a7ff4f0e101e8d1207fe1dc11d8d55
Tim Horton <hortont424@gmail.com>
2011-03-08 22:42:58 -0500

    center the logo

--- Sand/Sand/LobbyList.cs
+++ Sand/Sand/LobbyList.cs
@@ -28,11 +28,17 @@ namespace Sand
 
             _spriteBatch.Begin();
 
-            _spriteBatch.Draw(Storage.Sprite("SandLogo"), new Vector2(Game.Window.ClientBounds.Width * 0.2f, 20), Color.White);
+            var logoSprite = Storage.Sprite("SandLogo");
+
+            _spriteBatch.Draw(logoSprite,
+                              new Vector2(Game.Window.ClientBounds.Width * 0.5f - (logoSprite.Width * 0.5f), 20),
+                              Color.White);
 
             foreach(var gamer in Storage.networkSession.AllGamers)
             {
-                _spriteBatch.DrawString(Storage.Font("Calibri24"), string.Format("{0} ({1} ms)", gamer.Gamertag, gamer.RoundtripTime.TotalMilliseconds),
+                _spriteBatch.DrawString(Storage.Font("Calibri24"),
+                                        string.Format("{0} ({1} ms)", gamer.Gamertag,
+                                                      gamer.RoundtripTime.TotalMilliseconds),
                                         new Vector2(Game.Window.ClientBounds.Width * 0.22f, i++ * 30 + 120),
                                         Color.White);
             }
