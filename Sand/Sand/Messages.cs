bc9011e1a7b9f6df0e2f8e26ea73a5bba771b4c1
Tim Horton <hortont424@gmail.com>
2011-03-08 22:32:11 -0500

    fixing network

--- Sand/Sand/Messages.cs
+++ Sand/Sand/Messages.cs
@@ -15,13 +15,13 @@ namespace Sand
             Storage.packetWriter.Write((int) MessageTypes.UpdatePlayerPosition);
             Storage.packetWriter.Write(id);
             Storage.packetWriter.Write(player._position);
-            Storage.packetWriter.Write((double)player._angle);
+            Storage.packetWriter.Write((double) player._angle);
         }
 
         private static void ProcessUpdatePlayerPositionMessage(Player player)
         {
             player._position = Storage.packetReader.ReadVector2();
-            player._angle = (float)Storage.packetReader.ReadDouble();
+            player._angle = (float) Storage.packetReader.ReadDouble();
         }
 
         private static void UpdateClientStateFromServer(LocalNetworkGamer gamer)
@@ -63,8 +63,32 @@ namespace Sand
 
         private static void UpdateServerStateFromClients(LocalNetworkGamer gamer)
         {
-            if(gamer != null && gamer.IsDataAvailable)
+            while(gamer.IsDataAvailable)
             {
+                NetworkGamer sender;
+                gamer.ReceiveData(Storage.packetReader, out sender);
+
+                if(sender.IsLocal)
+                {
+                    continue;
+                }
+
+                while(Storage.packetReader.Position < Storage.packetReader.Length)
+                {
+                    var type = (MessageTypes) Storage.packetReader.ReadInt32();
+                    byte gamerId = Storage.packetReader.ReadByte();
+
+                    var player = sender.Tag as Player;
+
+                    switch(type)
+                    {
+                        case MessageTypes.UpdatePlayerPosition:
+                            ProcessUpdatePlayerPositionMessage(player);
+                            break;
+                        default:
+                            throw new ArgumentOutOfRangeException();
+                    }
+                }
             }
         }
 
@@ -72,13 +96,11 @@ namespace Sand
         {
             foreach(LocalNetworkGamer gamer in Storage.networkSession.LocalGamers)
             {
-                // TODO: write out updates from this client to the server!
-
                 if(!Storage.networkSession.IsHost)
                 {
                     SendUpdatePlayerPositionMessage(gamer.Tag as Player, gamer.Id);
 
-                    gamer.SendData(Storage.packetWriter, SendDataOptions.InOrder);
+                    gamer.SendData(Storage.packetWriter, SendDataOptions.InOrder, Storage.networkSession.Host);
                 }
             }
 
@@ -98,7 +120,7 @@ namespace Sand
 
                 var server = Storage.networkSession.Host as LocalNetworkGamer;
 
-                if (server != null)
+                if(server != null)
                 {
                     server.SendData(Storage.packetWriter, SendDataOptions.InOrder);
                 }
