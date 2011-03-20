903851fb068cc640811adbb63e1497899c7de6f3
Tim Horton <hortont424@gmail.com>
2011-03-20 16:08:32 -0400

    billboard and sandcolor

---
+++ Sand/Sand/Billboard.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Sand
+{
+    internal class Billboard : DrawableGameComponent
+    {
+        private Vector2 _origin;
+        private SpriteBatch _spriteBatch;
+        private Texture2D _texture;
+
+        public delegate void Action(object sender, object userInfo);
+
+        public Billboard(Game game, Vector2 origin, Texture2D texture)
+            : base(game)
+        {
+            _origin = origin;
+            _texture = texture;
+        }
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+
+            var sandGame = Game as Sand;
+
+            if (sandGame != null)
+            {
+                _spriteBatch = sandGame.SpriteBatch;
+            }
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            _spriteBatch.Draw(_texture, _origin, Color.White);
+        }
+    }
+}
---
+++ Sand/Sand/SandColor.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Sand
+{
+    class SandColor
+    {
+        public static void ToHSV(Color color, out double hue, out double saturation, out double value)
+        {
+            int max = Math.Max(color.R, Math.Max(color.G, color.B));
+            int min = Math.Min(color.R, Math.Min(color.G, color.B));
+
+            var sysColor = System.Drawing.Color.FromArgb(color.A, color.R, color.G, color.B);
+
+            hue = sysColor.GetHue();
+            saturation = (max == 0) ? 0 : 1d - (1d * min / max);
+            value = max / 255d;
+        }
+
+        public static Color FromHSV(double hue, double saturation, double value)
+        {
+            int hi = Convert.ToInt32(Math.Floor(hue / 60)) % 6;
+            double f = hue / 60 - Math.Floor(hue / 60);
+
+            value = value * 255;
+            int v = Convert.ToInt32(value);
+            int p = Convert.ToInt32(value * (1 - saturation));
+            int q = Convert.ToInt32(value * (1 - f * saturation));
+            int t = Convert.ToInt32(value * (1 - (1 - f) * saturation));
+
+            switch(hi)
+            {
+                case 0:
+                    return new Color(v, t, p);
+                case 1:
+                    return new Color(q, v, p);
+                case 2:
+                    return new Color(p, v, t);
+                case 3:
+                    return new Color(p, q, v);
+                case 4:
+                    return new Color(t, p, v);
+                default:
+                    return new Color(v, p, q);
+            }
+        }
+    }
+}
