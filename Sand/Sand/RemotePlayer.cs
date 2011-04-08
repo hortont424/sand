7600cf3281c161fda6f60117ba1c8a808110ea47
Tim Horton <hortont424@gmail.com>
2011-04-08 02:40:19 -0400

    split out local/remote player into their own files

---
+++ Sand/Sand/RemotePlayer.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Net;
+
+namespace Sand
+{
+    internal class RemotePlayer : Player
+    {
+        public RemotePlayer(Game game, NetworkGamer gamer) : base(game, gamer)
+        {
+        }
+    }
+}
